4a76e4dd6b144696c6115dc744b2db828b41f093
Harshit Kapadia <harshitkapadia1@gmail.com>
2018-03-18 23:51:41 +0100

    system of linear advection w/o b.c. and buggy plot routine

--- sbp_sat/get_system_data.m
+++ sbp_sat/get_system_data.m
@@ -1,12 +1,9 @@
 function[system_data] = get_system_data(n_equ)
 
-value_x = ones(n_equ)
-system_data.Ax = spdiags([value], [0], n_equ, n_equ);
+value_x = ones(n_equ);
+system_data.Ax = spdiags([value_x], [0], n_equ, n_equ);
 
-value_y = ones(n_equ)
-system_data.Ay = spdiags([value], [0], n_equ, n_equ);
-
-% system_data.Ax = full(system_data.Ax);
-% system_data.Ay = full(system_data.Ay);
+value_y = ones(n_equ);
+system_data.Ay = spdiags([value_y], [0], n_equ, n_equ);
 
 end
--- sbp_sat/sbp_collocated_2.m
+++ sbp_sat/sbp_collocated_2.m
@@ -1,4 +1,5 @@
-function[D, P, g?, S?] = get_system_data(n_equ)
+function[D, P] = sbp_collocated_2(N, h)
+% N - no. of grid cells & h - grid size
 
 d = ones(N+1, 1); % for diagonal entries of D matrix; length = len of diag
 D = spdiags([-d d], [-1 1], N+1, N+1);
@@ -11,11 +12,4 @@ P = spdiags([d], [0], N+1, N+1);
 P(1,1) = 1/2 ; P(N+1,N+1) = 1/2 ;
 P = h*P;
 
-% boundary at x=0 during t=0
-g = zeros(1, m);
-g(1,1) = solution(grid(1), 0);
-g(1,2) = solution2(grid(1), 0);
-
-S = zeros(N+1, m);
-S(1,1) = -1 * (u0(1,1)-g(1,1));
-S(1,2) = -1 * (u0(1,1)-g(1,1));
+end
