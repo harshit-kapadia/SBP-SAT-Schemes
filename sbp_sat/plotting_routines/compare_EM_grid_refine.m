f3c14743cdedbbb3f00ad2309977c5a09e6daafe
neerajsarna <neerajsarna@cscadmins-MacBook-Pro.local>
2019-10-08 17:06:58 +0200

    plotting routine for grid refine

--- sbp_sat/plotting_routines/compare_EM_grid_refine.m
+++ sbp_sat/plotting_routines/compare_EM_grid_refine.m
@@ -11,12 +11,53 @@
 % 1. odd
 % 2. char
 
-function f = compare_EM_grid_refine(test_case)
+clear all;
+
+test_case = 'gaussian_collision';
+nx_values1 = 10:5:35;
+error_test1 = grid_refine(test_case,nx_values1);
+convg1 = -log(error_test1(1:end-1)'./error_test1(2:end)')./log(nx_values1(1:end-1)./nx_values1(2:end));
+
+test_case = 'unsteady_lid_driven_cavity';
+nx_values2 = 10:5:40;
+error_test2 = grid_refine(test_case,nx_values2);
+convg2 = -log(error_test2(1:end-1)'./error_test2(2:end)')./log(nx_values2(1:end-1)./nx_values2(2:end));
+
+test_case = 'lid_driven_cavity';
+nx_values3 = 10:5:45;
+error_test3 = grid_refine(test_case,nx_values3);
+convg3 = -log(error_test3(1:end-1)'./error_test3(2:end)')./log(nx_values3(1:end-1)./nx_values3(2:end));
+
+test_case = 'heated_cavity';
+nx_values4 = [10:5:55];
+error_test4 = grid_refine(test_case,nx_values4);
+convg4 = -log(error_test4(1:end-1)'./error_test4(2:end)')./log(nx_values4(1:end-1)./nx_values4(2:end));
+
+plot(1:length(error_test1)-1,convg1,'-bo',...
+       1:length(error_test2)-1,convg2,'-r*',...
+       1:length(error_test3)-1,convg3,'-ms',...
+       1:length(error_test4)-1,convg4,'-kd',...
+       'markersize',10,'LineWidth',5,...
+       'MarkerFaceColor','b','MarkerFaceColor','r',...
+       'MarkerFaceColor','m','MarkerFaceColor','k');
+   
+%set(gca,'YScale','log'); 
+ylabel('convergence rate');
+xlabel('refinement cycle');
+legend('Test-1','Test-2','Test-3','Test-4','Location','best');   
+grid on;
+set(gca, 'FontSize', 20);
+
+base_output = strcat('/Users/neerajsarna/Dropbox/my_papers/Publications/Comparitive_BC/results/',test_case,'/');
+
+
+
+
+function f = grid_refine(test_case,nx_values)
 
 base_output = strcat('/Users/neerajsarna/Dropbox/my_papers/Publications/Comparitive_BC/results/',test_case,'/');
 
 %nx_values = 10:5:55;
-nx_values = [10:5:55,95,100];
 
 %% ids of macroscopic quantities
 filename_dvm = strcat('../DVM/',test_case,'/result_n100_DVM_20.txt');
@@ -38,21 +79,13 @@ for i = 1 : length(nx_values)
                         'result_n',num2str(nx_values(i)),'_M12','.txt');
    % filename_moments = strcat('../results/heated_cavity_char/result_M12.txt');
     result_mom{i} = dlmread(filename_moments,'\t');
-    result_mom{i} = interpolate_2D(result_mom{i},nx_values(i),X,Y);
-
-    
+    result_mom{i} = interpolate_2D(result_mom{i},nx_values(i),X,Y);   
 end
 
 [error] = compute_error(result_dvm,result_mom,PX);
 error_entropy = sqrt(dot(error,error,2));
 
 f = error_entropy;
-% loglog(error_entropy,'-o');
-% slope = 1;
-% plot_error_entropy(M_values,error_entropy,error_entropy_odd,slope);
-% 
-% filename = strcat(base_output,'EM');
-% print(filename,'-depsc');
 
 end
 
